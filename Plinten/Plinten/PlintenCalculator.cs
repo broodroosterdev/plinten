c424889397286e649f2627f74a63984aac8e1c17
broodroosterdev <myth.usa538@gmail.com>
2025-03-23 14:29:24 +0100

    Add files for solution

---
+++ Plinten/Plinten/PlintenCalculator.cs
@@ -0,0 +1,35 @@
+﻿class PlintenCalculator
+{
+    public static int CalculateSkirtingNeeded(double skirtingLength, List<double> requiredPieces)
+    {
+        // First sort the list of required pieces, from large to small.
+        var sortedCuts = requiredPieces.OrderDescending().ToList();
+        
+        // Store the amount of skirt that is not yet cut
+        var skirtRemaining = skirtingLength;
+        
+        // Count the amount of skirting needed
+        var skirtingNeeded = 1;
+        
+        while (sortedCuts.Count > 0)
+        {
+            var pieceIndex = sortedCuts.FindIndex(skirt => skirt <= skirtRemaining);
+            if (pieceIndex == -1)
+            {
+                skirtingNeeded++;
+                skirtRemaining = skirtingLength;
+            }
+            else
+            {
+                skirtRemaining -= sortedCuts[pieceIndex];
+                sortedCuts.RemoveAt(pieceIndex);
+            }
+        }
+
+        return skirtingNeeded;
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
+
+
---
+++ Plinten/Plinten/PlintenCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+public class PlintenCalculatorTests
+{
+    [Theory]
+    [InlineData(new double[] {100, 100, 100}, 1)]
+    [InlineData(new double[] {100, 150, 100}, 2)]
+    [InlineData(new double[] {240, 60, 100}, 2)]
+    [InlineData(new double[] {250, 60, 100}, 2)]
+    [InlineData(new double[] {250, 60, 250}, 3)]
+    public void TestCalculateSkirtingNeeded(double[] requiredPieces, int expectedSkirtingNeeded)
+    {
+        const double skirtingLength = 300;
+        Assert.Equal(expectedSkirtingNeeded, PlintenCalculator.CalculateSkirtingNeeded(skirtingLength, requiredPieces.ToList()));
+    }
+}
