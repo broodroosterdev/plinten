9ad912a41ff3e31952fa119c973de93d133b37dc
broodroosterdev <myth.usa538@gmail.com>
2025-03-23 14:39:16 +0100

    Add comments explaining the algorithm used and the test data

--- Plinten/Plinten/PlintenCalculator.cs
+++ Plinten/Plinten/PlintenCalculator.cs
@@ -1,9 +1,11 @@
-﻿class PlintenCalculator
+﻿namespace Plinten;
+
+class PlintenCalculator
 {
     public static int CalculateSkirtingNeeded(double skirtingLength, List<double> requiredPieces)
     {
         // First sort the list of required pieces, from large to small.
-        var sortedCuts = requiredPieces.OrderDescending().ToList();
+        var sortedPieces = requiredPieces.OrderDescending().ToList();
         
         // Store the amount of skirt that is not yet cut
         var skirtRemaining = skirtingLength;
@@ -11,25 +13,30 @@
         // Count the amount of skirting needed
         var skirtingNeeded = 1;
         
-        while (sortedCuts.Count > 0)
+        // Loop through the sorted pieces, until all pieces have been cut
+        while (sortedPieces.Count > 0)
         {
-            var pieceIndex = sortedCuts.FindIndex(skirt => skirt <= skirtRemaining);
+            // Go through the list from large to small, until a piece is found that will fit on the remaining space of the skirt.
+            var pieceIndex = sortedPieces.FindIndex(skirt => skirt <= skirtRemaining);
+            
+            // An index of -1 indicates no piece was found that will fit the remaining space.
+            // This means the remaining space is too small to be used, so a new skirt is needed.
             if (pieceIndex == -1)
             {
                 skirtingNeeded++;
                 skirtRemaining = skirtingLength;
             }
+            // If we did find a piece that matches:
             else
             {
-                skirtRemaining -= sortedCuts[pieceIndex];
-                sortedCuts.RemoveAt(pieceIndex);
+                // Subtract the length from the remaining space and remove the piece from the list.
+                skirtRemaining -= sortedPieces[pieceIndex];
+                sortedPieces.RemoveAt(pieceIndex);
             }
         }
-
+        
+        // When we finish from the loop, it means all pieces have found its way on a skirt.
+        // We can then return the total amount of skirts needed
         return skirtingNeeded;
     }
 }
-
-// See https://aka.ms/new-console-template for more information
-
-
--- Plinten/Plinten/PlintenCalculatorTests.cs
+++ Plinten/Plinten/PlintenCalculatorTests.cs
@@ -1,11 +1,17 @@
-﻿using Xunit;
+﻿using Plinten;
+using Xunit;
 
 public class PlintenCalculatorTests
 {
     [Theory]
+    // Tests from assignment
+    [InlineData(new double[] {240, 60}, 1)]
+    [InlineData(new double[] {240, 60, 100}, 2)]
+    
+    // Extra tests to make sure solution is robust
     [InlineData(new double[] {100, 100, 100}, 1)]
     [InlineData(new double[] {100, 150, 100}, 2)]
-    [InlineData(new double[] {240, 60, 100}, 2)]
+    [InlineData(new double[] {100, 50, 50, 50, 100}, 2)]
     [InlineData(new double[] {250, 60, 100}, 2)]
     [InlineData(new double[] {250, 60, 250}, 3)]
     public void TestCalculateSkirtingNeeded(double[] requiredPieces, int expectedSkirtingNeeded)
